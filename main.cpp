8e488ccfc86e1c2c481bddc61b702732fb6dc753
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-09-29 19:05:47 +0530

    MODIFY: main.cpp file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <fstream>
-#include <filesystem>
-#include <map>
+
+struct fileStructure {
+  std::string name;
+  long long byte_size = 0;
+};
+
+class analyser{
+  private:
+  std::vector <fileStructure> files;
+  public:
+  void populate_test_data(){
+    std::cout<<"--poplating vector with test data---\n";
+    files.clear();
+    
 
 
+  }
+};
 int main(){
   return 0;
 }
