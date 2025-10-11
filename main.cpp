368a062d172d0aa1cf0f2fe9debe1e9d9ff90e4d
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-10-11 15:51:08 +0530

    MODIFY: main.cpp file

--- main.cpp
+++ main.cpp
@@ -127,7 +127,7 @@ class cliManager{ //cli managing class for display of main menu
         } else if(command == "fsearch"){
             a.searchfile();
         }else if(command == "tcred"){
-            printf("\n\tCredits :\ntecnolgd\nDocumentation at https://c.com\n");
+            printf("\n\tCredits :\nAuthor: tecnolgd\nDocumentation at https://c.com\n");
         }else if (command == "help") {
             std::cout << "\n\t-- Available commands --\n populate  - reading /fetching file names and byte size\n sortbyte  - display the files sorted based on byte size\n report    - summarize key findings regarding file data\n fsearch   - to sreach for a file based on its name\n maxbyte   - gives the max byte file\n exit/quit - terminate from the application\n tcred - tool credits and doc details\n";
         } else {
