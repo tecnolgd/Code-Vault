05dc87a371b07ce43ce0d24a1d7d18c875408e19
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-10-25 19:08:52 +0530

    MODIFY: cmd.cpp file

--- cmd.cpp
+++ cmd.cpp
@@ -3,14 +3,37 @@
 
 //#include "main.cpp"
 
-int main(int argc, char* argv[]){
-    if(argc<2){
+int main(int argc, char* argv[]){ //to get the number of words used in the command to interpret the command given by the user.
+    if(argc<2){ //error check
         std::cout << "------------------------------------------" << std::endl;
         std::cout << "\tCodeVault - C++ Code Analyzer (Beta)" << std::endl;
         std::cout << "Error: At least two words needed\n" << std::endl;
         std::cout<<"( Note: Try using \"help\")"<<std::endl;
         return 1;
     }
-    std::string command = argv[1];
-    
+    std::string command = argv[1]; //first word is the command
+    analyzer a; //object of class analyser
+
+    if(command=="populate"){
+        if (argc != 3) {
+            std::cout << "ERROR: 'analyze' requires a directory path." << std::endl;
+            std::cout << "Use: ./codevault analyze <path_to_directory>" << std::endl;
+            return 1;
+        }
+        std::string path = argv[2];
+        analyzer.populate(path);
+        
+    }
+    else if(command =="report"){
+        std::cout << "Running full report. Analyzing current directory ('.') first." << std::endl;
+        analyzer.populate("."); // Load data from current folder
+        analyzer.report();
+    }
+    else if (command == "fsearch"){
+        if (argc != 3) {
+            std::cout << "ERROR: 'search' requires a filename." << std::endl;
+            std::cout << "Use: ./codevault search <filename_to_find>" << std::endl;
+            return 1;
+        }
+    }
 }
