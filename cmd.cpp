421e48d6c25fd224a42bb306d7ed35a37d157661
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-10-25 19:00:23 +0530

    MODIFY: cmd.cpp file

--- cmd.cpp
+++ cmd.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 #include <string>
 
-#include "main.cpp"
+//#include "main.cpp"
 
+int main(int argc, char* argv[]){
+    if(argc<2){
+        std::cout << "------------------------------------------" << std::endl;
+        std::cout << "\tCodeVault - C++ Code Analyzer (Beta)" << std::endl;
+        std::cout << "Error: At least two words needed\n" << std::endl;
+        std::cout<<"( Note: Try using \"help\")"<<std::endl;
+        return 1;
+    }
+    std::string command = argv[1];
+    
+}
