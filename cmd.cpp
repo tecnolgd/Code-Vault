756e96ea2c0c71368d8d9e096db847cc681222d7
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-10-27 14:59:06 +0530

    MODIFY: cmd.cpp file

--- cmd.cpp
+++ cmd.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]){ //to get the number of words used in the comma
         if (argc != 3) {// 3, since the populate needs the filename or directory as the 3rd word in the command.
             std::cout << "ERROR: 'analyze' requires a directory path." << std::endl;
             std::cout << "Use: ./codevault analyze <path_to_directory>" << std::endl;
-            return 1;
+            return 2;
         }
         std::string path = argv[2];// words start from 0 ,so 2is the 3rd word i.e., the filename.
         a.populate(path);
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]){ //to get the number of words used in the comma
         if (argc != 3) {
             std::cout << "ERROR: 'search' requires a filename." << std::endl;
             std::cout << "Use: ./codevault search <filename_to_find>" << std::endl;
-            return 1;
+            return 3;
         }
         a.analyze("."); // need explanation !!!
         std::string fileName = argv[2]; // The file name to find is argv[2]
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]){ //to get the number of words used in the comma
     }
     else {
         std::cout << "ERROR: Unknown command '" << command << ". Check \' help \' for available commands." << std::endl;
-        return 1; //default statement for error check.
+        return -1; //default statement for error check.
     }
     return 0 //program success
 }
