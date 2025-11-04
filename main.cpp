96b18a1c5afbe2d630f139a2331b9268d495decb
tecnolgd <203659863+tecnolgd@users.noreply.github.com>
2025-11-04 18:47:50 +0530

    MODIFY: main.cpp file

--- main.cpp
+++ main.cpp
@@ -167,7 +167,7 @@ class cliManager{ //cli managing class for display of main menu
 };
 
 // --- Execution Focus: Simplified Main Loop ---
-int main() {
+/*int main() {
     analyzer a; //object of class analyser
     cliManager cli; //object of class cliManager
     cli.runterminal(a); //passing address of class analyser object 'a' to function 'runterminal' 
@@ -175,9 +175,10 @@ int main() {
     /* Flow of the cli.runterminal(&a); ->  object a  address--> passed to runterminal()function as argument --> later derefenced to get data or value from it --> runterminal() is executed with cli i.e., ' cli.runterminal(&a) '*/
 
     //Basically, class analyser is the argument for function runterminal() which runs inside class cliManager.
-    return 0;
+    //return 0;
 
-}
+//}
+
 
 
 
